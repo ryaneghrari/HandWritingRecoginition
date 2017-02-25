4b6046dda4995db79e4b1a48dfad0ac43ff21cf1
Raidon <ryan.eghrari@richmond.edu>
2017-02-25 12:01:36 -0500

    adding starter code

---
+++ ClassifierMain.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class ClassifierMain here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClassifierMain  {
+
+    public static void main(String[] arg) {
+        
+        ClassifierWindow window = new ClassifierWindow();
+    }
+}
